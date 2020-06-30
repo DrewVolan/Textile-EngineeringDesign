91f59a5ec46fc2e8b2260f1826bf1f5871ec5d80
DrewVolan <vandrewv16@gmail.com>
2020-06-30 21:20:41 +0300

    Характеристики компьютера при авторизации

--- EngineeringDesign/AuthorizationForm.cs
+++ EngineeringDesign/AuthorizationForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 
@@ -37,7 +38,8 @@ namespace EngineeringDesign
             RegistrationButton.Enabled = true;
             infoLabel.Visible = false;
         }
-
+        MySqlConnection conn = DB.GetDBConnection();
+        bool isConnected = false;
         private async void EnterButton_Click(object sender, EventArgs e)
         {
             sqlConnection = new SqlConnection(connectionPathMain);
@@ -57,6 +59,7 @@ namespace EngineeringDesign
                             switch (Convert.ToString(sdr["Role"]))
                             {
                                 case "Заказчик":
+                                    GetInformationAboutComputer();
                                     CustomerForm regC = new CustomerForm();
                                     regC.Login = textBox1.Text;
                                     regC.Left = this.Left;
@@ -65,6 +68,7 @@ namespace EngineeringDesign
                                     this.Hide();
                                     break;
                                 case "Менеджер":
+                                    GetInformationAboutComputer();
                                     ManagerForm managerForm = new ManagerForm();
                                     managerForm.Login = textBox1.Text;
                                     managerForm.Left = this.Left;
@@ -73,6 +77,7 @@ namespace EngineeringDesign
                                     this.Hide();
                                     break;
                                 case "Кладовщик":
+                                    GetInformationAboutComputer();
                                     StorekeeperForm storekeeperForm = new StorekeeperForm();
                                     storekeeperForm.Login = textBox1.Text;
                                     storekeeperForm.Left = this.Left;
@@ -81,6 +86,7 @@ namespace EngineeringDesign
                                     this.Hide();
                                     break;
                                 case "Администратор":
+                                    GetInformationAboutComputer();
                                     AdministratorForm administratorForm = new AdministratorForm();
                                     administratorForm.Login = textBox1.Text;
                                     administratorForm.Left = this.Left;
@@ -89,6 +95,7 @@ namespace EngineeringDesign
                                     this.Hide();
                                     break;
                                 case "Директор":
+                                    GetInformationAboutComputer();
                                     DirectorForm directorForm = new DirectorForm();
                                     directorForm.Login = textBox1.Text;
                                     directorForm.Left = this.Left;
@@ -143,6 +150,93 @@ namespace EngineeringDesign
                 infoLabel.Text = "Авторизация заблокирована на 5 секунд!";
                 infoLabel.Visible = true;
                 countInvalidAttempts = 3;
+            }
+        }
+
+        private void GetInformationAboutComputer()
+        {
+            string MachineName = Environment.MachineName;
+            string UserName = Environment.UserName;
+            string OSVersion = Environment.OSVersion.ToString();
+            string CPUArch = Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE");
+            string CPUModel = Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER");
+            string CPUCoreCount = Environment.ProcessorCount.ToString();
+
+            Console.WriteLine("Название ПК: {0}", Environment.MachineName);
+            Console.WriteLine("Имя пользователя: {0}", Environment.UserName);
+            Console.WriteLine("Операционная система (номер версии):  {0}", Environment.OSVersion);
+            Console.WriteLine("Разрядность процессора:  {0}", Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE"));
+            Console.WriteLine("Модель процессора:  {0}", Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER"));
+            Console.WriteLine("Число процессоров:  {0}", Environment.ProcessorCount);
+
+            try
+            {
+                conn.Open();
+                MessageBox.Show("БД работает");
+                isConnected = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (isConnected == true)
+            {
+                try
+                {
+                    string sql = $"SELECT `DateRecordHardwareInfo` FROM `Users` WHERE `Id` = '1'"; // Будем же по id определять?
+                    MySqlCommand cmd = conn.CreateCommand();
+                    cmd.CommandText = sql;
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+
+
+                    reader.Read(); // Warning
+                    var lastDateInfoCheck = reader["DateRecordHardwareInfo"];
+                    reader.Close();
+                    conn.Close();
+
+                    Console.WriteLine("Последняя дата из БД:  {0}", lastDateInfoCheck);
+
+                    DateTime today = DateTime.Now;
+                    DateTime endOfMonth = new DateTime(today.Year, today.Month + 1, 1).AddDays(-1);
+
+                    Console.WriteLine($"Конец месяца - {endOfMonth}");
+                    Console.WriteLine($"в записи о дате - {lastDateInfoCheck}");
+
+                    Console.WriteLine($"проверка  - {today.Day} and {endOfMonth.Day}");
+
+
+                    // Проверка на null (когда даты тупо нет в БД изначально) или конец месяца
+                    if (lastDateInfoCheck is DBNull || today.Day == endOfMonth.Day)
+                    {
+                        conn.Open();
+
+                        DateTime dateUpdateInfo = today;
+
+
+                        string insert = $"UPDATE `Users` SET " +
+                            $"`PcName` = '{MachineName}', " +
+                            $"`PcUserName` = '{UserName}', " +
+                            $"`Os` = '{OSVersion}', " +
+                            $"`CpuBitRate` = '{CPUArch}', " +
+                            $"`CpuModel` = '{CPUModel}', " +
+                            $"`CPUCoreCount` = '{CPUCoreCount}', " +
+                            $"`DateRecordHardwareInfo` = '{dateUpdateInfo.ToString("yyyy-MM-dd HH:mm:ss")}' " +
+                            $"WHERE `Login` = 'admin'";
+
+                        MySqlCommand inserInfo = new MySqlCommand(insert, conn);
+
+                        inserInfo.ExecuteNonQuery();
+                        conn.Close();
+
+                        MessageBox.Show("Успешно обновилось!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
         }
 
